4a9d61d84fca106356c9d3ad2eb51b455e97c134
Lia Janzelj <liajanzelj@gmail.com>
2020-05-16 18:36:33 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+mood = "happy"
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Hope you feel better soon!")
+elif mood == "excited":
+    print("Keep it up and make it work!")
+elif mood == "relaxed":
+    print("Float in your inner peace :)")
+else:
+    print("I don't recognize this mood.")
