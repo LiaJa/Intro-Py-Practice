375ea164d0917b2a05e1da3909e9744cb39aae23
Lia Janzelj <liajanzelj@gmail.com>
2020-05-16 19:09:46 +0200

    add guess_game

---
+++ guess_game.py
@@ -0,0 +1,7 @@
+secret = int("14")
+guess = int(input("Please enter a number between 0 and 15:"))
+
+if guess == secret:
+        print("Congrats, your guess is correct!")
+else:
+        print("Sorry, please try with another guess.")
